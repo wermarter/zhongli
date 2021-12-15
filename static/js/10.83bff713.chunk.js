f1fb0ba84fb5058bc0c9b4855cae07d47323585c
Ha Minh Chien <wermarter@gmail.com>
2021-12-15 14:53:43 +0700

    Updates

---
+++ static/js/10.83bff713.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient_admin=this.webpackJsonpclient_admin||[]).push([[10],{345:function(n,t,e){"use strict";e.r(t);e(0);var i=e(1);t.default=function(){return Object(i.jsx)("h1",{children:"Sorry, page not found"})}}}]);
+//# sourceMappingURL=10.83bff713.chunk.js.map
--- static/js/10.ac1a9fb2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient_admin=this.webpackJsonpclient_admin||[]).push([[10],{344:function(n,t,e){"use strict";e.r(t);e(0);var i=e(1);t.default=function(){return Object(i.jsx)("h1",{children:"Sorry, page not found"})}}}]);
-//# sourceMappingURL=10.ac1a9fb2.chunk.js.map
